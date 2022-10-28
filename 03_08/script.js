068d2be10196294a558d67c644ab8019523a70a9
Thounda Craig <thoundac94@gmail.com>
2022-10-28 12:10:09 -0400

    change values using method & console log

--- 03_08/script.js
+++ 03_08/script.js
@@ -23,3 +23,10 @@ const backpack = {
 
 console.log("The backpack object:", backpack);
 console.log("The pocketNum value:", backpack.pocketNum);
+
+// Methods with JS changes the value within the browser - in real time; not changing the JS code itself when debugging. However, methods can also be accessed via the script as well to update values.
+
+console.log("Left Before:", backpack.strapLength.left);
+backpack.newStrapLength(10, 15);
+// Notice the values have changed due to passing data to method
+console.log("Left After:", backpack.strapLength.left);
