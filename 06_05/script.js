4ced0e13db2de669138b659ebec03a77e7111cc1
Thounda Craig <thoundac94@gmail.com>
2022-11-06 17:34:01 -0500

    set constant to demo error when trying to reassign value

--- 06_05/script.js
+++ 06_05/script.js
@@ -3,12 +3,12 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
  */
 
-let color = "purple";
+const color = "purple"; // defining the constant prevents the variable from being changed later in script
 
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
 
-color = "skyblue";
+color = "skyblue"; // this statement will throw error because 'color' already set and cannot be changed
 
 function headingColor() {
   let color = "blue";
