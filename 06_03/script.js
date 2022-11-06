2e73f43a83f6563cc5cfe9e30893445c74a7680b
Thounda Craig <thoundac94@gmail.com>
2022-11-06 15:31:10 -0500

    examples of JS setting inline styles

--- 06_03/script.js
+++ 06_03/script.js
@@ -8,12 +8,17 @@ var color = "purple";
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
 
+// change the color which will be read by next set of lines, below - JS read TOP/DOWN
+color = "skyblue";
+
 document.querySelector(".right").style.backgroundColor = color;
 document.querySelector(".right .color-value").innerHTML = color;
 
-// function headingColor() {
-//   color = "blue";
-//   document.querySelector(".title").style.color = color;
-// }
+// Example of a function to  set inline style
+function headingColor() {
+  color = "blue";
+  document.querySelector(".title").style.color = color;
+}
 
-// headingColor();
+// Call function to set color
+headingColor();
