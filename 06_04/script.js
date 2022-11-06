46ab66126ed255a99ec8ee2dd3daf2a023df1e6a
Thounda Craig <thoundac94@gmail.com>
2022-11-06 16:25:27 -0500

    examples of block scope vars in js script

--- 06_04/script.js
+++ 06_04/script.js
@@ -3,7 +3,8 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
  */
 
-var color = "purple";
+// globally scoped variable
+let color = "purple";
 
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
@@ -11,11 +12,14 @@ document.querySelector(".left .color-value").innerHTML = color;
 color = "skyblue";
 
 function headingColor() {
-  color = "blue";
+  let titleColor = "blue"; // block scoped var within function using 'let'
   document.querySelector(".title").style.color = color;
+  console.log("inside:", titleColor); // locally scope var is defined inside function
 }
 
 headingColor();
+
+console.log("outside:", titleColor); // locally scope var called outside of function; result in undefined
 
 document.querySelector(".right").style.backgroundColor = color;
 document.querySelector(".right .color-value").innerHTML = color;
