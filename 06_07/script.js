b879ebd69b092be732b93b0114d6913ccae38040
Thounda Craig <thoundac94@gmail.com>
2022-11-06 19:54:11 -0500

    example of assignment and comparism operators

--- 06_07/script.js
+++ 06_07/script.js
@@ -3,12 +3,15 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Relational_operators
  */
 
+// single equals symbol 'assigns value'
 let a = 5;
 let b = 4;
 
 console.log(`let a: ${a} (${typeof a})`);
 console.log(`let b: ${b} (${typeof b})`);
 
+// double equals symbol indicates LOOSE 'comparism' operation
+// trippe equals or bang symbol and two equals indicate STRICt 'comparism' operation
 if (a == b) {
   console.log(`Match! let a and let b are the same value.`);
 } else {
