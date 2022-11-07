401a8d93eab2b368279b784b27eede978ecb56c1
Thounda Craig <thoundac94@gmail.com>
2022-11-06 19:00:57 -0500

    list data types for via console log

--- 06_06/script.js
+++ 06_06/script.js
@@ -5,37 +5,42 @@
 
 // String:
 let stringDemo = "A string of text.";
-console.log("String:", stringDemo);
+console.log("String:", stringDemo, "typeof:", typeof stringDemo);
 
-// Numbes:
+// Numbers:
 let integerDemo = 4;
-console.log("Integer:", integerDemo);
+console.log("Integer:", integerDemo, "typeof:", typeof integerDemo);
 
 let floatDemo = 5.6;
-console.log("Floating point number:", floatDemo);
+console.log("Floating point number:", floatDemo, "typeof:", typeof floatDemo);
 
 // Boolean:
 let booleanDemo = true;
-console.log("Boolean value:", booleanDemo);
+console.log("Boolean value:", booleanDemo, "typeof:", typeof booleanDemo);
 
 // Null value (nothing):
 let nullDemo = null;
-console.log("Null value:", nullDemo);
+console.log("Null value:", nullDemo, "typeof:", typeof nullDemo);
 
 // Undefined:
 let undefinedDemo;
-console.log("Undefined:", undefinedDemo);
+console.log("Undefined:", undefinedDemo, "typeof:", typeof undefinedDemo);
 
 let undefinedAssignedDemo = undefined;
-console.log("Undefined assigned:", undefinedAssignedDemo);
+console.log(
+  "Undefined assigned:",
+  undefinedAssignedDemo,
+  "typeof:",
+  typeof undefinedAssignedDemo
+);
 
 // Object:
 const objectDemo = {
   dance: "Mambo",
   number: 5,
 };
-console.log("Object:", objectDemo);
+console.log("Object:", objectDemo, "typeof:", typeof objectDemo);
 
 // Array:
 const arrayDemo = ["tango", "foxtrot", "waltz", "rumba", "bolero"];
-console.log("Array:", arrayDemo);
+console.log("Array:", arrayDemo, "typeof:", typeof arrayDemo);
