5cb9d3bdf13ea5be4c0b0fbe4aff83f386a601b9
Thounda Craig <thoundac94@gmail.com>
2022-11-09 13:17:10 -0500

    basic examples of defining & viewing arrays

--- 07_02/script.js
+++ 07_02/script.js
@@ -7,4 +7,24 @@ let item = "flashlight";
 
 const collection = ["Piggy", item, 5, true];
 
+// display array via console log
 console.log(collection);
+// Display count or length of items within the array
+console.log(collection.length);
+
+// Call item within array starting with index zero
+console.log(collection[1]); // this calls the 2nd item within the array
+
+// Update or change an item within an array
+collection[2] = "camera";
+console.log(collection);
+
+// Add a new item to the end of the array
+collection[collection.length] = "new item";
+console.log(collection);
+
+// Set item futher down within an array - items 5 thru 8 will be set to 'undefinded'
+collection[9] = "at the end";
+console.log(collection[9]);
+console.log(collection[5]);
+console.log(collection[8]);
