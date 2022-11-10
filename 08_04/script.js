1e17c29fe9a47794f64c9864634e7a237e79826a
Thounda Craig <thoundac94@gmail.com>
2022-11-10 15:06:22 -0500

    define ARROW function example

--- 08_04/script.js
+++ 08_04/script.js
@@ -10,6 +10,8 @@ const greenPack = {
   pocketNum: 3,
 };
 
+// Define a ARROW Function
+/* Arrow functions can only be called after they have been declared. Thus, impulsing a stricter way of writing JS code by using arrow functions. Second, you can't use arrow functions when declaring methods in an object. Inside an object if you have a method you need to use a proper anonymous function declaration. The arrow function simply doesn't work in that context */
 const addPack = (currentPack) => {
   const newArticle = document.createElement("article");
   newArticle.innerHTML = `
