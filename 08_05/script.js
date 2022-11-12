3d8f928c54b86ae6a849d98e3fad2488fce97dad
Thounda Craig <thoundac94@gmail.com>
2022-11-12 15:38:38 -0500

    'this' examples of Declaration & Arrow functions

--- 08_05/script.js
+++ 08_05/script.js
@@ -5,7 +5,7 @@
  */
 
 // Define a default volume for the window (the entire DOM):
-window.volume = 20;
+window.volume = 20; // This is the value that the IIEF will display b/c IIEF triggers instantly
 
 const greenPack = {
   name: "Frog Pack",
@@ -13,12 +13,19 @@ const greenPack = {
   volume: 8,
   pocketNum: 3,
   newVolume: function (volume) {
-    console.log("this.volume in the method:", this.volume);
-    this.volume = volume;
+    console.log("this.volume in the method:", this.volume); // the value of 8 currently assigned
+    this.volume = volume; // the 'this' keyword now reassigns the value from 8 to 5 to volume
     console.log("this.volume after update:", this.volume);
-    // (function () {
-    //   console.log("this.volume in nested function:", this.volume);
-    // })();
+    // IIEF to showcase issue using the 'this' keyword within an object
+    (function () {
+      // Function Declarations are hoisted to the global scope - out of the object
+      console.log("this.volume in nested function 'Declaration':", this.volume); // Look as the top of script for value assignment
+    })();
+    // Below, we use an ARROW function which now produce the correct results - value of 5
+    (() => {
+      // Function Declarations are hoisted to the global scope - out of the object
+      console.log("this.volume in nested 'Arrow' function:", this.volume); // Look as the top of script for value assignment
+    })();
   },
 };
 
