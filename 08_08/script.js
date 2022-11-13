9343989194c1f311611625285458fb4afba088e7
Thounda Craig <thoundac94@gmail.com>
2022-11-13 17:00:00 -0500

    return currency & locale values from one function to another

--- 08_08/script.js
+++ 08_08/script.js
@@ -8,15 +8,33 @@
  * @link http://www.lingoes.net/en/translator/langcode.htm
  */
 
-const tipCalculator = (sum, percentage) => {
+// create arrow function to format the 'sum' value - returning those results back to tipCalculator function
+const formatter = (locale, currency, value) => {
+  let formattedValue = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(value);
+  // expected output: set based on - inputted - locale
+  return formattedValue;
+};
+
+// 'sum' is going to be passed up to the 'formatter' function for processing then returned, properly, formatted
+const tipCalculator = (sum, percentage, locale, currency) => {
   let tip = sum * (percentage / 100);
   let total = sum + tip;
   console.log(`
-      Sum before tip: ${sum}
+      Sum before tip: ${formatter(locale, currency, sum)}
       Tip percentage: ${percentage}%
-      Tip:            ${tip.toFixed(2)}
-      Total:          ${total.toFixed(2)}
+      Tip:            ${formatter(locale, currency, tip)}
+      Total:          ${formatter(locale, currency, total)}
     `);
 };
 
-tipCalculator(29.95, 18);
+// Locale for Germany
+tipCalculator(29.95, 18, "de-DE", "EUR");
+// Locale for Japanesse (YEN)
+tipCalculator(29.95, 18, "ja-JP", "JPY");
+// Locale for Norwegian (NOK)
+tipCalculator(29.95, 18, "nb-NO", "NOK");
+// Locale for English (United States)
+tipCalculator(29.95, 18, "en-US", "USD");
