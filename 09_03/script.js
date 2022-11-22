94b86475f5b35f8efc4e716e84840cb0d875b8cd
Thounda Craig <thoundac94@gmail.com>
2022-11-22 13:55:34 -0500

    create eventListener on button to change status

--- 09_03/script.js
+++ 09_03/script.js
@@ -2,14 +2,20 @@
  * Event listeners
  * @link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
  * @link https://developer.mozilla.org/en-US/docs/Web/Events
+ *
+ * Definition: An EventListener is exactly what it sounds like: A method added to a target, usually an element, that listens for a specific event and then calls back a function when that event is detected.
  */
+
+// Import module to hook in logic from exported file to local file - script.js
 import backpackObjectArray from "./components/data.js";
 
+// Create copy or instance of the original array to work with in code, below - leaving the original array 'as is'
 const backpackList = backpackObjectArray.map((backpack) => {
-  let backpackArticle = document.createElement("article");
-  backpackArticle.classList.add("backpack");
-  backpackArticle.setAttribute("id", backpack.id);
+  let backpackArticle = document.createElement("article"); // creating DOM element to inject into HTML on render
+  backpackArticle.classList.add("backpack"); // defining the CSS class to include in html once rendered to browser
+  backpackArticle.setAttribute("id", backpack.id); // setting an id to each item - also recommended when looping thru array
 
+  // At end of code snippet - a button element was added to work with the JS code to explain demo
   backpackArticle.innerHTML = `
     <figure class="backpack__image">
       <img src=${backpack.image} alt="" loading="lazy" />
@@ -39,9 +45,24 @@ const backpackList = backpackObjectArray.map((backpack) => {
     <button class="lid-toggle">Open lid</button>
   `;
 
+  // Grab the button for each interaction (article) which is where the EventList will be set
+  const button = backpackArticle.querySelector(".lid-toggle");
+  const status = backpackArticle.querySelector(".backpack__lid span");
+
+  // Create EventListener to grab event from button on 'click' and change 'status'
+  button.addEventListener("click", (event) => {
+    console.log(event);
+    // Toggle the status
+    status.innerText === "open"
+      ? (status.innerText = "closed")
+      : (status.innerText = "open");
+  });
+
+  // passing back results from the function / method
   return backpackArticle;
 });
 
+// Grabbing the element in the HTML DOM where we want to inject the template literal content
 const main = document.querySelector(".maincontent");
 
 backpackList.forEach((backpack) => {
